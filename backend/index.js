b929080598bdc59bae76922774c188fa71374124
Cyb3R0xSASA <elmohtarftop@gmail.com>
2025-08-05 14:56:45 +0300

    Modify .env structure

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import path from "path";
 import express from "express";
-import dotenv from "dotenv";
+import 'dotenv/config'
 import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -8,9 +8,11 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
-dotenv.config();
+import morgan from "morgan";
+
 const port = process.env.PORT || 3000;
 const app = express();
+app.use(morgan('dev'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import 'dotenv/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:3000",
-      "/uploads/": "http://localhost:3000",
+      "/api/": process.env.BASE_URL,
+      "/uploads/": process.env.BASE_URL,
     },
   },
 });
