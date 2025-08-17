cc4ffd86374ab6fc27e3dd227f003768cecbd75c
Yousef Bakr Zaki Alsaidi <youbakrzaki@gmail.com>
2025-08-17 12:33:11 +0300

    fix app bug

--- backend/index.js
+++ backend/index.js
@@ -17,9 +17,9 @@ const corsOptions = {
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: ["Content-Type, Authorization"],
 };
-app.use(cors(corsOptions));
 const port = process.env.PORT || 3000;
 const app = express();
+app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
